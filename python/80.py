99e721ac4d0dce504b47719f4588bf1e0a823888
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-03-11 02:23:57 +0600

    remove obsolete implementations of merge, remove element, two sum, and remove duplicates algorithms

---
+++ python/80.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeDuplicates(self, nums):
+        if len(nums) <= 2:
+            return len(nums)
+
+        index = 2
+        for i in range(2, len(nums)):
+            if nums[i] != nums[index - 2]:
+                nums[index] = nums[i]
+                index += 1
+
+        return index
