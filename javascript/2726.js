3db8f65041d32d1f087b7966ccfea1c45bdd856e
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-03-21 23:20:30 +0600

    2726

---
+++ javascript/2726.js
@@ -0,0 +1,37 @@
+class Calculator {
+    constructor(value) {
+        this.result = value;
+    }
+
+    add(value) {
+        this.result += value;
+        return this; // Return current instance for method chaining
+    }
+
+    subtract(value) {
+        this.result -= value;
+        return this; // Return current instance for method chaining
+    }
+
+    multiply(value) {
+        this.result *= value;
+        return this; // Return current instance for method chaining
+    }
+
+    divide(value) {
+        if (value === 0) {
+            throw "Division by zero is not allowed"; // Handle division by zero
+        }
+        this.result /= value;
+        return this; // Return current instance for method chaining
+    }
+
+    power(value) {
+        this.result = Math.pow(this.result, value);
+        return this; // Return current instance for method chaining
+    }
+
+    getResult() {
+        return this.result;
+    }
+}
