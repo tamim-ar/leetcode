f86f7b72991b96394882ccf1b5cca598a9845d47
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-03-21 23:42:14 +0600

    274

---
+++ python/274.py
@@ -0,0 +1,7 @@
+class Solution:
+    def hIndex(self, citations):
+        citations.sort(reverse=True)
+        for i, c in enumerate(citations):
+            if c < i + 1:
+                return i
+        return len(citations)
