89590b7ff3a4d8a02678c1e0a1941a38f70add20
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-04-15 21:41:27 +0600

    1251

--- sql/1251.sql
+++ sql/1251.sql
@@ -1,5 +1,8 @@
 SELECT p.product_id,
-       ROUND(SUM(p.price * u.units) / SUM(u.units), 2) AS average_price
+       ROUND(
+           IFNULL(SUM(p.price * u.units), 0) / IFNULL(SUM(u.units), 1),
+           2
+       ) AS average_price
 FROM Prices p
 LEFT JOIN UnitsSold u
   ON p.product_id = u.product_id
