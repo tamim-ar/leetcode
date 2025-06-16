fbaebb92f9e006c4c46d07c3c5054a35c1b52a69
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-06-16 22:27:18 +0600

    remove unnecessary blank lines in data processing functions

--- python/2883.py
+++ python/2883.py
@@ -1,6 +1,4 @@
-
 import pandas as pd
-
 
 def dropMissingData(students: pd.DataFrame) -> pd.DataFrame:
   students.dropna(subset=['name'], inplace=True)
--- python/2885.py
+++ python/2885.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 def renameColumns(students: pd.DataFrame) -> pd.DataFrame:
   return students.rename(
       columns={
