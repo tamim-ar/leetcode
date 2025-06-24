7ec44ef8a2ca611c932734c48a1b68597b7f1644
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-06-24 16:35:26 +0600

    24

---
+++ python/24.py
@@ -0,0 +1,18 @@
+class Solution:
+    def swapPairs(self, head):
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+
+        while head and head.next:
+            first = head
+            second = head.next
+
+            prev.next = second
+            first.next = second.next
+            second.next = first
+
+            prev = first
+            head = first.next
+
+        return dummy.next
