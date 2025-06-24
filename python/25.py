d480048295339ec3ef99b0e824c93781dda12886
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-06-24 16:36:20 +0600

    25

---
+++ python/25.py
@@ -0,0 +1,30 @@
+class Solution:
+    def reverseKGroup(self, head, k):
+        def get_kth(curr, k):
+            while curr and k > 0:
+                curr = curr.next
+                k -= 1
+            return curr
+
+        dummy = ListNode(0)
+        dummy.next = head
+        group_prev = dummy
+
+        while True:
+            kth = get_kth(group_prev, k)
+            if not kth:
+                break
+            group_next = kth.next
+
+            prev, curr = kth.next, group_prev.next
+            while curr != group_next:
+                tmp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = tmp
+
+            tmp = group_prev.next
+            group_prev.next = kth
+            group_prev = tmp
+
+        return dummy.next
