c844d212434da6e899e30cea46e175b0e5f745c4
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-06-30 21:51:13 +0600

    Refactor findLHS function to use Counter for improved readability and performance

--- python/594.py
+++ python/594.py
@@ -1,10 +1,13 @@
+from collections import Counter
+
+
 class Solution:
     def findLHS(self, nums: List[int]) -> int:
-        count = {}
-        for num in nums:
-            count[num] = count.get(num, 0) + 1
-        max_len = 0
-        for key in count:
-            if key + 1 in count:
-                max_len = max(max_len, count[key] + count[key + 1])
-        return max_len
+        count = Counter(nums)
+        max_length = 0
+
+        for num in count:
+            if num + 1 in count:
+                max_length = max(max_length, count[num] + count[num + 1])
+
+        return max_length
