c23eee9c233be4a6f9cdc86c3036e13082b87b0e
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-07-01 00:34:09 +0600

    Refactor minRemoveToMakeValid function to simplify logic and improve readability

--- python/1249.py
+++ python/1249.py
@@ -1,20 +1,15 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
+        s = list(s)
         stack = []
-        indices_to_remove = set()
-        
-        # Mark indices of invalid parentheses
-        for i, char in enumerate(s):
-            if char == '(':
+        for i, c in enumerate(s):
+            if c == '(':
                 stack.append(i)
-            elif char == ')':
+            elif c == ')':
                 if stack:
                     stack.pop()
                 else:
-                    indices_to_remove.add(i)
-        
-        # Add remaining opening parentheses indices
-        indices_to_remove.update(stack)
-        
-        # Build result string excluding marked indices
-        return ''.join(char for i, char in enumerate(s) if i not in indices_to_remove)
+                    s[i] = ''
+        while stack:
+            s[stack.pop()] = ''
+        return ''.join(s)
