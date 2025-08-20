c315a62847ce6a08f7ab2b6c7a974971ba7f49a1
Tamim Ahasan RIjon <tamimahasan.ar@gmail.com>
2025-08-20 23:28:09 +0600

    1277. Count Square Submatrices with All Ones

---
+++ solutions/1277. Count Square Submatrices with All Ones/1277.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countSquares(self, matrix: List[List[int]]) -> int:
+        m, n = len(matrix), len(matrix[0])
+        dp = [[0] * n for _ in range(m)]
+        res = 0
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 1:
+                    if i == 0 or j == 0:
+                        dp[i][j] = 1
+                    else:
+                        dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
+                    res += dp[i][j]
+        return res
