8b5925d55bc9164f9c1194e6db09a09f05defb71
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-08-25 23:40:56 +0600

    58. Length of Last Word

---
+++ solutions/58. Length of Last Word/58.py
@@ -0,0 +1,12 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        i = len(s) - 1
+        # skip trailing spaces
+        while i >= 0 and s[i] == ' ':
+            i -= 1
+        length = 0
+        # count length of last word
+        while i >= 0 and s[i] != ' ':
+            length += 1
+            i -= 1
+        return length
