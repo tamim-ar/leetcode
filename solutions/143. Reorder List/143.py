dffea4c013c1cb0541d91b624d490b322d945d16
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-08-26 19:12:24 +0600

    143. Reorder List

---
+++ solutions/143. Reorder List/143.py
@@ -0,0 +1,21 @@
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        if not head or not head.next:
+            return
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        prev, curr = None, slow.next
+        slow.next = None
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
