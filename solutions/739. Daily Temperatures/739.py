4b7eb4861b1db4378e2676f7665dd9b6700d686e
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-09-03 20:50:41 +0600

    739. Daily Temperatures

---
+++ solutions/739. Daily Temperatures/739.py
@@ -0,0 +1,13 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        answer = [0] * n
+        stack = []  # stores indices
+
+        for i, temp in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < temp:
+                idx = stack.pop()
+                answer[idx] = i - idx
+            stack.append(i)
+
+        return answer
