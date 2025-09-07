2ce8cd13f954abc7a16747bac80dbec71ab29231
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-09-07 17:23:54 +0600

    1657. Determine if Two Strings Are Close

---
+++ solutions/1657. Determine if Two Strings Are Close/1657.py
@@ -0,0 +1,7 @@
+from collections import Counter
+
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        if set(word1) != set(word2):
+            return False
+        return sorted(Counter(word1).values()) == sorted(Counter(word2).values())
