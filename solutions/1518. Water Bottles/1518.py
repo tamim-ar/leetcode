01f86d41fbc842aa3736ddbe0f0a050175d460f6
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-10-01 22:52:07 +0600

    1518. Water Bottles

---
+++ solutions/1518. Water Bottles/1518.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
+        total = numBottles
+        empty = numBottles
+        while empty >= numExchange:
+            new = empty // numExchange
+            total += new
+            empty = empty % numExchange + new
+        return total
