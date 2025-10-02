3cb296d92a721e1f017e250a84872bd1a6b39bd8
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-10-03 00:08:07 +0600

    3100. Water Bottles II

---
+++ solutions/3100. Water Bottles II/3100.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxBottlesDrunk(self, numBottles: int, numExchange: int) -> int:
+        res = numBottles
+        empty = numBottles
+        while empty >= numExchange:
+            empty -= numExchange
+            numExchange += 1
+            res += 1
+            empty += 1
+        return res
