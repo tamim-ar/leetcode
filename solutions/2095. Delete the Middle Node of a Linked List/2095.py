1069055ca30c3c6e59253bf6b42d36e2ad3920ff
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-10-10 22:32:37 +0600

    2095. Delete the Middle Node of a Linked List

---
+++ solutions/2095. Delete the Middle Node of a Linked List/2095.py
@@ -0,0 +1,10 @@
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return None
+        slow, fast = head, head.next.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        slow.next = slow.next.next
+        return head
