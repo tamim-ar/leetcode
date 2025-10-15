ea3f507bff2a8b04730c64211f80bdfed2e897a4
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-10-15 20:05:26 +0600

    3350. Adjacent Increasing Subarrays Detection II

--- solutions/3350. Adjacent Increasing Subarrays Detection II/3350.py
+++ solutions/3350. Adjacent Increasing Subarrays Detection II/3350.py
@@ -1,12 +1,18 @@
 class Solution:
     def maxIncreasingSubarrays(self, nums: List[int]) -> int:
         n = len(nums)
-        inc = [1] * n
+        left = [1] * n
+        right = [1] * n
+
         for i in range(1, n):
             if nums[i] > nums[i - 1]:
-                inc[i] = inc[i - 1] + 1
+                left[i] = left[i - 1] + 1
+
+        for i in range(n - 2, -1, -1):
+            if nums[i] < nums[i + 1]:
+                right[i] = right[i + 1] + 1
 
         res = 0
-        for i in range(1, n):
-            res = max(res, min(inc[i], inc[i - 1]))
+        for i in range(n - 1):
+            res = max(res, min(left[i], right[i + 1]))
         return res
