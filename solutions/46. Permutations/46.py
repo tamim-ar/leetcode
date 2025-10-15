5aebae5eb6c343e66901fd4f01ad719474189007
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-10-15 20:06:03 +0600

    46. Permutations

---
+++ solutions/46. Permutations/46.py
@@ -0,0 +1,11 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def backtrack(path, remaining):
+            if not remaining:
+                res.append(path)
+                return
+            for i in range(len(remaining)):
+                backtrack(path + [remaining[i]], remaining[:i] + remaining[i+1:])
+        backtrack([], nums)
+        return res
