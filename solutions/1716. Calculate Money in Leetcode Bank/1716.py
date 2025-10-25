72fe4dacd113be7793003cca6f28d419af9297e0
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-10-25 23:45:18 +0600

    1716. Calculate Money in Leetcode Bank

--- solutions/1716. Calculate Money in Leetcode Bank/1716.py
+++ solutions/1716. Calculate Money in Leetcode Bank/1716.py
@@ -1,14 +1,14 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
-        weeks = n // 7
-        days = n % 7
         total = 0
+        week = 0
+        day = 1
         
-        # Sum of full weeks
-        total += weeks * (28 + 7 * (weeks - 1) // 2 * 7)
-        
-        # Sum of remaining days
-        total += days * (weeks + (days + 1) // 2) if days else 0
-        total += sum(weeks + i + 1 for i in range(days))
+        for i in range(1, n + 1):
+            total += week + day
+            day += 1
+            if day > 7:
+                week += 1
+                day = 1
         
         return total
