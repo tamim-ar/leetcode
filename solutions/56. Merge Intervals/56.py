7d8ff34c8a37f8fd4cb11837a2448821e6deea81
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-11-04 23:59:25 +0600

    56. Merge Intervals

---
+++ solutions/56. Merge Intervals/56.py
@@ -0,0 +1,15 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        if not intervals:
+            return []
+        
+        intervals.sort(key=lambda x: x[0])
+        merged = [intervals[0]]
+        
+        for current in intervals[1:]:
+            if current[0] <= merged[-1][1]:
+                merged[-1][1] = max(merged[-1][1], current[1])
+            else:
+                merged.append(current)
+        
+        return merged
