b18878e25213a667a5ba6402e72aadccf203cc80
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-11-05 20:50:49 +0600

    3321. Find X-Sum of All K-Long Subarrays II

--- solutions/3321. Find X-Sum of All K-Long Subarrays II/3321.py
+++ solutions/3321. Find X-Sum of All K-Long Subarrays II/3321.py
@@ -1,24 +1,46 @@
-from collections import Counter
-import heapq
+from sortedcontainers import SortedList
+
 
 class Solution:
-    def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
-        n = len(nums)
-        ans = []
-        cnt = Counter(nums[:k])
+  # Same as 3318. Find X-Sum of All K-Long Subarrays I
+  def findXSum(self, nums: list[int], k: int, x: int) -> list[int]:
+    ans = []
+    windowSum = 0
+    count = collections.Counter()
+    top = SortedList()  # the top x elements
+    bot = SortedList()  # the rest of the elements
 
-        def get_x_sum():
-            freq = sorted(cnt.items(), key=lambda y: (-y[1], -y[0]))
-            top = freq[:x]
-            return sum(v * c for v, c in top)
+    def update(num: int, freq: int) -> None:
+      """Updates the count of num by freq and the window sum accordingly."""
+      nonlocal windowSum
+      if count[num] > 0:  # Clean up the old count.
+        if [count[num], num] in bot:
+          bot.remove([count[num], num])
+        else:
+          top.remove([count[num], num])
+          windowSum -= num * count[num]
+      count[num] += freq
+      if count[num] > 0:
+        bot.add([count[num], num])
 
-        ans.append(get_x_sum())
+    for i, num in enumerate(nums):
+      update(num, 1)
+      if i >= k:
+        update(nums[i - k], -1)
+      # Move the bottom elements to the top if needed.
+      while bot and len(top) < x:
+        countB, b = bot.pop()
+        top.add([countB, b])
+        windowSum += b * countB
+      # Swap the bottom and top elements if needed.
+      while bot and bot[-1] > top[0]:
+        countB, b = bot.pop()
+        countT, t = top.pop(0)
+        bot.add([countT, t])
+        top.add([countB, b])
+        windowSum += b * countB
+        windowSum -= t * countT
+      if i >= k - 1:
+        ans.append(windowSum)
 
-        for i in range(k, n):
-            left = nums[i - k]
-            cnt[left] -= 1
-            if cnt[left] == 0:
-                del cnt[left]
-            cnt[nums[i]] += 1
-            ans.append(get_x_sum())
-        return ans
+    return ans
