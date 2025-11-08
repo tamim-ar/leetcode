53f5a285eae8bfdef033aa8227c90c55bb4c6362
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-11-08 15:46:27 +0600

    78. Subsets

---
+++ solutions/78. Subsets/78.py
@@ -0,0 +1,11 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        def backtrack(start, subset):
+            res.append(subset[:])
+            for i in range(start, len(nums)):
+                subset.append(nums[i])
+                backtrack(i + 1, subset)
+                subset.pop()
+        backtrack(0, [])
+        return res
