b1e3b2380f8c49d5aa2ff8040052fd58059414b6
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-11-12 00:00:19 +0600

    87. Scramble String

--- solutions/87. Scramble String/87.py
+++ solutions/87. Scramble String/87.py
@@ -1,13 +1,18 @@
+from functools import lru_cache
+
 class Solution:
     def isScramble(self, s1: str, s2: str) -> bool:
-        if s1 == s2:
-            return True
-        if sorted(s1) != sorted(s2):
+        @lru_cache(None)
+        def dfs(a, b):
+            if a == b:
+                return True
+            if sorted(a) != sorted(b):
+                return False
+            n = len(a)
+            for i in range(1, n):
+                if (dfs(a[:i], b[:i]) and dfs(a[i:], b[i:])) or \
+                   (dfs(a[:i], b[-i:]) and dfs(a[i:], b[:-i])):
+                    return True
             return False
         
-        n = len(s1)
-        for i in range(1, n):
-            if (self.isScramble(s1[:i], s2[:i]) and self.isScramble(s1[i:], s2[i:])) or \
-               (self.isScramble(s1[:i], s2[-i:]) and self.isScramble(s1[i:], s2[:-i])):
-                return True
-        return False
+        return dfs(s1, s2)
