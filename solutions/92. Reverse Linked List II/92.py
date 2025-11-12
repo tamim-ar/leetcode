615a79e628534b459aec73402029bb0f14c57567
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-11-12 23:07:41 +0600

    92. Reverse Linked List II

---
+++ solutions/92. Reverse Linked List II/92.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        if not head or left == right:
+            return head
+        dummy = ListNode(0, head)
+        prev = dummy
+        for _ in range(left - 1):
+            prev = prev.next
+        curr = prev.next
+        for _ in range(right - left):
+            temp = curr.next
+            curr.next = temp.next
+            temp.next = prev.next
+            prev.next = temp
+        return dummy.next
