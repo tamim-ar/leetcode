2e11b6d9d22f8cf50a189c215db8043408bfae3e
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-11-13 12:23:45 +0600

    3228. Maximum Number of Operations to Move Ones to the End

--- solutions/3228. Maximum Number of Operations to Move Ones to the End/3228.py
+++ solutions/3228. Maximum Number of Operations to Move Ones to the End/3228.py
@@ -1,7 +1,12 @@
 class Solution:
-    def maxOperations(self, s: str) -> int:
-        one = ans = 0
-        for c in s:
-            if c=='1': one+=1
-            else: ans+=one
-        return ans
+  def maxOperations(self, s: str) -> int:
+    ans = 0
+    ones = 0
+
+    for i, c in enumerate(s):
+      if c == '1':
+        ones += 1
+      elif i + 1 == len(s) or s[i + 1] == '1':
+        ans += ones
+
+    return ans
