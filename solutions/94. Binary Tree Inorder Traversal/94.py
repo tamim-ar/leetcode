fd16bd0fa044bb3372703c558154aee79309fe37
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-11-13 12:26:21 +0600

    94. Binary Tree Inorder Traversal

---
+++ solutions/94. Binary Tree Inorder Traversal/94.py
@@ -0,0 +1,10 @@
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        def dfs(node):
+            if not node: return
+            dfs(node.left)
+            res.append(node.val)
+            dfs(node.right)
+        dfs(root)
+        return res
