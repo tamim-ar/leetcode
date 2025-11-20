13f7db817f88db6db1234f0851c13fff3e27a633
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-11-20 15:45:04 +0600

    103. Binary Tree Zigzag Level Order Traversal

---
+++ solutions/103. Binary Tree Zigzag Level Order Traversal/103.py
@@ -0,0 +1,31 @@
+from collections import deque
+from typing import Optional, List
+
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        q = deque([root])
+        res = []
+        left_to_right = True
+        while q:
+            level_size = len(q)
+            level = []
+            for _ in range(level_size):
+                node = q.popleft()
+                level.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            if not left_to_right:
+                level.reverse()
+            res.append(level)
+            left_to_right = not left_to_right
+        return res
