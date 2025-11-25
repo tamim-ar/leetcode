a128a42278b2535f5ba235d2aa4968eb695271e2
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-11-25 23:59:25 +0600

    1015. Smallest Integer Divisible by K

---
+++ solutions/1015. Smallest Integer Divisible by K/1015.py
@@ -0,0 +1,10 @@
+class Solution:
+    def smallestRepunitDivByK(self, k: int) -> int:
+        if k % 2 == 0 or k % 5 == 0:
+            return -1
+        n = 0
+        for i in range(1, k + 1):
+            n = (n * 10 + 1) % k
+            if n == 0:
+                return i
+        return -1
