5c8e1b612b083bfd2a53b6c194ac133dc6ea7a96
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2025-12-25 23:50:56 +0600

    3075. Maximize Happiness of Selected Children

---
+++ solutions/3075. Maximize Happiness of Selected Children/3075.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
+        happiness.sort(reverse=True)
+        ans = 0
+        for i in range(k):
+            if happiness[i] > i:
+                ans += happiness[i] - i
+        return ans
