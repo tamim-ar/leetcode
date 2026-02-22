c4e0b47dbda612b25c02cea61b8658b17ae865a3
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2026-02-22 21:16:20 +0600

    868. Binary Gap

---
+++ solutions/868. Binary Gap/868.py
@@ -0,0 +1,15 @@
+class Solution:
+    def binaryGap(self, n: int) -> int:
+        last = -1     # last position of '1'
+        pos = 0
+        ans = 0
+
+        while n > 0:
+            if n & 1:
+                if last != -1:
+                    ans = max(ans, pos - last)
+                last = pos
+            n >>= 1
+            pos += 1
+
+        return ans
