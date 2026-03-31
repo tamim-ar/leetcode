af7907a5bd760abb2c6f4088b5d5e86f4fc09abd
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2026-03-31 12:52:41 +0600

    130. Surrounded Regions

---
+++ solutions/130. Surrounded Regions/130.py
@@ -0,0 +1,36 @@
+from typing import List
+
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        if not board or not board[0]:
+            return
+        
+        m, n = len(board), len(board[0])
+        
+        def dfs(r, c):
+            if r < 0 or c < 0 or r >= m or c >= n or board[r][c] != 'O':
+                return
+            board[r][c] = 'T'
+            dfs(r+1, c)
+            dfs(r-1, c)
+            dfs(r, c+1)
+            dfs(r, c-1)
+        
+        for i in range(m):
+            if board[i][0] == 'O':
+                dfs(i, 0)
+            if board[i][n-1] == 'O':
+                dfs(i, n-1)
+        
+        for j in range(n):
+            if board[0][j] == 'O':
+                dfs(0, j)
+            if board[m-1][j] == 'O':
+                dfs(m-1, j)
+        
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'
+                elif board[i][j] == 'T':
+                    board[i][j] = 'O'
