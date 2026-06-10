e45e58e4acc69a7ec8b3e54f092aa06c82045989
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2026-06-11 02:58:04 +0600

    3691. Maximum Total Subarray Value II

--- solutions/3691. Maximum Total Subarray Value II/3691.py
+++ solutions/3691. Maximum Total Subarray Value II/3691.py
@@ -1,71 +1,50 @@
-from typing import List
-import heapq
+class SparseTableRMQ:
+    def __init__(self, data: List[int]):
+        self.n = len(data)
+        self.max_log = self.n.bit_length() + 1
+        self.f_max = [[0] * self.max_log for _ in range(self.n)]
+        self.f_min = [[0] * self.max_log for _ in range(self.n)]
+
+        self.lg = [0] * (self.n + 1)
+        for i in range(2, self.n + 1):
+            self.lg[i] = self.lg[i >> 1] + 1
+
+        for i in range(self.n):
+            self.f_max[i][0] = data[i]
+            self.f_min[i][0] = data[i]
+
+        for j in range(1, self.max_log):
+            for i in range(self.n - (1 << j) + 1):
+                self.f_max[i][j] = max(
+                    self.f_max[i][j - 1], self.f_max[i + (1 << (j - 1))][j - 1]
+                )
+                self.f_min[i][j] = min(
+                    self.f_min[i][j - 1], self.f_min[i + (1 << (j - 1))][j - 1]
+                )
+
+    def query_max(self, l: int, r: int) -> int:
+        k = self.lg[r - l + 1]
+        return max(self.f_max[l][k], self.f_max[r - (1 << k) + 1][k])
+
+    def query_min(self, l: int, r: int) -> int:
+        k = self.lg[r - l + 1]
+        return min(self.f_min[l][k], self.f_min[r - (1 << k) + 1][k])
+
 
 class Solution:
     def maxTotalValue(self, nums: List[int], k: int) -> int:
         n = len(nums)
-
-        lg = (n + 1).bit_length()
-        st_max = [nums[:]]
-        st_min = [nums[:]]
-
-        j = 1
-        while (1 << j) <= n:
-            prev_max = st_max[j - 1]
-            prev_min = st_min[j - 1]
-            length = 1 << (j - 1)
-
-            cur_max = [0] * (n - (1 << j) + 1)
-            cur_min = [0] * (n - (1 << j) + 1)
-
-            for i in range(len(cur_max)):
-                cur_max[i] = max(prev_max[i], prev_max[i + length])
-                cur_min[i] = min(prev_min[i], prev_min[i + length])
-
-            st_max.append(cur_max)
-            st_min.append(cur_min)
-            j += 1
-
-        log = [0] * (n + 1)
-        for i in range(2, n + 1):
-            log[i] = log[i // 2] + 1
-
-        def range_value(l, r):
-            p = log[r - l + 1]
-            mx = max(st_max[p][l], st_max[p][r - (1 << p) + 1])
-            mn = min(st_min[p][l], st_min[p][r - (1 << p) + 1])
-            return mx - mn
-
-        def best_split(l, r):
-            best_pos = l
-            best_val = -1
-
-            for i in range(l, r + 1):
-                v = range_value(i, r)
-                if v > best_val:
-                    best_val = v
-                    best_pos = i
-
-            return best_pos, best_val
-
-        heap = []
-
-        for r in range(n):
-            pos, val = best_split(0, r)
-            heapq.heappush(heap, (-val, 0, pos, r))
+        st = SparseTableRMQ(nums)
+        pq = []
+        for l in range(n):
+            val = st.query_max(l, n - 1) - st.query_min(l, n - 1)
+            heappush(pq, (-val, l, n - 1))
 
         ans = 0
-
         for _ in range(k):
-            val, L, pos, r = heapq.heappop(heap)
+            val, l, r = heappop(pq)
             ans += -val
-
-            if L <= pos - 1:
-                npos, nval = best_split(L, pos - 1)
-                heapq.heappush(heap, (-nval, L, npos, r))
-
-            if pos + 1 <= r:
-                npos, nval = best_split(pos + 1, r)
-                heapq.heappush(heap, (-nval, pos + 1, npos, r))
-
+            if r > l:
+                val = st.query_max(l, r - 1) - st.query_min(l, r - 1)
+                heappush(pq, (-val, l, r - 1))
         return ans
