9e60404964b4206250f15d18fd11036d5ed16a5a
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2026-07-20 22:22:52 +0600

    1260. Shift 2D Grid

---
+++ solutions/1260. Shift 2D Grid/1260.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def shiftGrid(self, grid, k):
+        """
+        :type grid: List[List[int]]
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        m = len(grid)
+        n = len(grid[0])
+        total = m * n
+
+        k %= total
+        ans = [[0] * n for _ in range(m)]
+
+        for i in range(m):
+            for j in range(n):
+                idx = i * n + j
+                new_idx = (idx + k) % total
+                r = new_idx // n
+                c = new_idx % n
+                ans[r][c] = grid[i][j]
+
+        return ans
