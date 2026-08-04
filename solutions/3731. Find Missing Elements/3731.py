38600a6c76442c1db326adbf46915bd1725d3293
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2026-08-05 01:58:17 +0600

    3731. Find Missing Elements

---
+++ solutions/3731. Find Missing Elements/3731.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findMissingElements(self, nums: List[int]) -> List[int]:
+        s = set(nums)
+        ans = []
+
+        for x in range(min(nums), max(nums) + 1):
+            if x not in s:
+                ans.append(x)
+
+        return ans
