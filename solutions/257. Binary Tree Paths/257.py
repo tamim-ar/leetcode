18ac3951d39cc8c68e146b5ebfd9850b0781d243
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2026-08-25 23:28:26 +0600

    257. Binary Tree Paths

---
+++ solutions/257. Binary Tree Paths/257.py
@@ -0,0 +1,22 @@
+class Solution:
+    def binaryTreePaths(self, root):
+        result = []
+
+        def dfs(node, path):
+            if not node:
+                return
+
+            path += str(node.val)
+
+            # Leaf node
+            if not node.left and not node.right:
+                result.append(path)
+                return
+
+            path += "->"
+
+            dfs(node.left, path)
+            dfs(node.right, path)
+
+        dfs(root, "")
+        return result
