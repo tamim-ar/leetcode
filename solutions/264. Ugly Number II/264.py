11333df2f46637b9efa14336c85c8a6292f93e93
Tamim Ahasan Rijon <tamimahasan.ar@gmail.com>
2026-09-04 20:50:21 +0600

    264. Ugly Number II

---
+++ solutions/264. Ugly Number II/264.py
@@ -0,0 +1,22 @@
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        ugly = [1] * n
+        i2 = i3 = i5 = 0
+
+        for i in range(1, n):
+            next_ugly = min(
+                ugly[i2] * 2,
+                ugly[i3] * 3,
+                ugly[i5] * 5
+            )
+
+            ugly[i] = next_ugly
+
+            if next_ugly == ugly[i2] * 2:
+                i2 += 1
+            if next_ugly == ugly[i3] * 3:
+                i3 += 1
+            if next_ugly == ugly[i5] * 5:
+                i5 += 1
+
+        return ugly[-1]
